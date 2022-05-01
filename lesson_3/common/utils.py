4f63054b168c90bf85f8be0c6164e476cc3cc1a6
optikrus <optikrus61@gmail.com>
2022-05-01 14:22:43 +0300

    add get_message & send_message

--- lesson_3/common/utils.py
+++ lesson_3/common/utils.py
@@ -1,7 +1,20 @@
+import json
 
-def get_message():
-    pass
+from consts import ENCODING, MAX_PACKAGE_LENGTH
 
 
-def send_message():
-    pass
+def get_message(client):
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        str_json_response = encoded_response.decode(ENCODING)
+        response = json.loads(str_json_response)
+        if isinstance(response, dict):
+            return response
+        raise ValueError
+    raise ValueError
+
+
+def send_message(socket, message):
+    str_json_data = json.dumps(message)
+    encoded_message = str_json_data.encode(ENCODING)
+    socket.send(encoded_message)

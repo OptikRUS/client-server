38937eaafc949054b7ce195c30934436e7f662ec
optikrus <optikrus61@gmail.com>
2022-05-01 15:13:56 +0300

    add valid and server

--- lesson_3/common/utils.py
+++ lesson_3/common/utils.py
@@ -1,6 +1,6 @@
 import json
 
-from consts import ENCODING, MAX_PACKAGE_LENGTH
+from consts import ENCODING, MAX_PACKAGE_LENGTH, ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, ERROR
 
 
 def get_message(client):
@@ -18,3 +18,12 @@ def send_message(socket, message):
     str_json_data = json.dumps(message)
     encoded_message = str_json_data.encode(ENCODING)
     socket.send(encoded_message)
+
+
+def valid_message(message):
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message and USER in message:
+        return {RESPONSE: 200}
+    return {
+        RESPONSE: 400,
+        ERROR: 'BAD REQUEST'
+    }

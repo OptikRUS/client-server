240b60992c96892bd497204c01624a5840b58621
optikrus <optikrus61@gmail.com>
2022-05-17 14:19:42 +0300

    add logging for client

--- lesson_5/client.py
+++ lesson_5/client.py
@@ -1,9 +1,13 @@
 import sys
 import json
+import logging
 from socket import socket, AF_INET, SOCK_STREAM
 
 from common.consts import DEFAULT_IP_ADDRESS, DEFAULT_PORT
 from common.utils import create_presence, valid_answer, get_message, send_message
+from logs.config import config_client_log
+
+CLIENT_LOGGER = logging.getLogger('client')
 
 
 def main():
@@ -11,12 +15,13 @@ def main():
         server_address = sys.argv[1]
         server_port = int(sys.argv[2])
         if not 1024 < server_port < 65535:
-            raise ValueError
+            CLIENT_LOGGER.critical(f"Неверный номер порта: {server_port} Значение <port> в диапазоне от 1024 до 65635")
+        CLIENT_LOGGER.info(f"Запущен клиент с адресом {server_address} и портом {server_port}")
+
     except IndexError:
         server_address = DEFAULT_IP_ADDRESS
         server_port = DEFAULT_PORT
-    except ValueError:
-        print("Значение <port> в диапазоне от 1024 до 65635")
+        CLIENT_LOGGER.debug(f"Будут использованы стандартные адрес {server_address} и порт {server_port}")
         sys.exit(1)
 
     CLIENT_SOCKET = socket(AF_INET, SOCK_STREAM)
@@ -26,9 +31,9 @@ def main():
 
     try:
         answer = valid_answer(get_message(CLIENT_SOCKET))
-        print(answer)
+        CLIENT_LOGGER.info(f'Ответ от сервера {answer}')
     except (ValueError, json.JSONDecodeError):
-        print("Сообщение с сервера не декодировано")
+        CLIENT_LOGGER.critical("Не удалось декодировать сообщение")
 
 
 if __name__ == '__main__':
--- lesson_5/logs/config/config_server_log.py
+++ lesson_5/logs/config/config_server_log.py
@@ -6,7 +6,7 @@ import logging.handlers
 from common.consts import LOGGING_LEVEL
 
 
-FORMATTER = logging.Formatter("%(asctime)-5s %(levelname)-10s %(filename)-23s %(message)s")
+FORMATTER = logging.Formatter("%(asctime)s %(levelname)-10s %(filename)-23s %(message)s")
 
 PATH = os.path.join(os.path.dirname(__file__), "server.log")
 
@@ -19,3 +19,9 @@ LOGGER = logging.getLogger('server')
 LOGGER.addHandler(STREAM_HANDLER)
 LOGGER.addHandler(LOG_FILE)
 LOGGER.setLevel(LOGGING_LEVEL)
+
+if __name__ == '__main__':
+    LOGGER.critical('Критическая ошибка')
+    LOGGER.error('Ошибка')
+    LOGGER.debug('Отладочная информация')
+    LOGGER.info('Информационное сообщение')
